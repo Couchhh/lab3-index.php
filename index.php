ec9d7679b111f2ea176297bdce2002a5e121a32a
Sophia Palma <119684820+Couchhh@users.noreply.github.com>
2023-02-06 22:40:25 -0500

    Lab 3: created folder

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Sophia Palma">
+        <meta name="description" content="climate change awareness">
+    <!--this is ironic because I love to fish-->
+    </head>
+        <body>
+        <body style="background-color:#ADD8E6">
+        <title>How industrial-scale fishing is impacting the ocean</title>
+        <nav>
+          <a href="https://spalma1.w3.uvm.edu/cs008/">Home</a>
+          <a href="form.php">Important Survey</a>
+        </nav>
+        <main>
+            <h1>The Big Deal About <strong>Fishing</strong></h1>
+            <section>
+                <h2>what is industrial-scale fishing</h2>
+                <p>Fishing is negatively impacting our environment, and I'm not talking about the kind where you go out with your pals to see who can catch the biggest fish. I'm talking about real big boats dragging thousands of fish in massive nets. These boats in particular are decreasing the population of dolphin, sea turtle, and shark population due to accidentally catching them up. On top of this, they are secreting loads of CO2 into the atmosphere from the amount of fuel they eat up from these trips. </p>
+            </section>
+            <section>
+                <h2>impact in the ocean</h2>
+                <p>According to National Geographic, more than 55 percent of the worlds ocean has been fished upon by these large scale boats. The term bottom trawling refers to the practice of dragging big nets across the ocean floor, and sometimes they catch some unexpected friends. These products are what they call bycatch, which are mainly turtles, dolphins, and sharks. All of these marine life are endangered with one of the factors being fishing. On top of this, sharks are apex predators, which means the absence of them can mean a disruption in the food chain. If bottom trawling continues, it could lead to catastrophic changes with the marine food chain.</p>
+            </section>
+         <figure>
+            <img alt="sharks" src="Images/sharks_20.png">
+            <figcaption> A shark I saw that could be killed due to being caught in bottom trawling.</figcaption>
+        </figure>
+            <section>
+                <h2>what debris from fishing can due to the wildlife</h2>
+                <ol>
+                    <li>Thrown out net can catch unwanted fish in the water</li>
+                    <li>Fishing gear debris can bring unwanted parasites that can negatively impact the ecosystem</li>
+                    <li>Certain fishing gear, for example, can damage coral reefs and other sensitive habitats</li>
+                    <li><a href="https://www.nature.org/en-us/newsroom/ca-ocean-plastic/">source</a></li>
+                </ol>
+            </section>
+            <table style="background-color:#CAEEC2">
+
+            <tr>
+                <th><strong>type of garbage</strong></th>
+            </tr>
+            <tr>
+                <td>Plastic bag</td>
+                <td>Plastic bottle</td>
+                <td>Food containers and cutlery</td>
+                <td>Wrappers</td>
+                <td>Synthetic rope</td>
+                <td>Fishing items</td>
+                <td>Plastic caps/lids</td>
+                <td> Industrial packing</td>
+                <td>Glass bottles</td>
+                <td>Drink cans</td>
+            </tr>
+            <tr>
+            <th><strong>percent in the ocean</strong></th>
+            </tr>
+            <tr>
+                <td>14.1%</td>
+                <td>11.9%</td>
+                <td>9.4%</td>
+                <td>9.1%</td>
+                <td>7.9%</td>
+                <td>7.6%</td>
+                <td>6.1%</td>
+                <td>3.4%</td>
+                <td>3.4%</td>
+                <td>3.2%</td>
+            </tr>
+            <th><strong>source</strong></th>
+            <tr>
+                <td><a href="https://www.statista.com/chart/25056/waste-items-polluting-oceans/">source</a></td>
+            </tr>
+            </table>
+        </main>
+        <footer>
+            <p><a href="https://spalma1.w3.uvm.edu/cs008/">Site map (Main index)</a></p>
+           </footer>
+        </body>
+        </html>
+
+                
+
+
+        
