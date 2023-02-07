25db8983091b72c53d43899b6025b3a8cd658b17
Sophia Palma <119684820+Couchhh@users.noreply.github.com>
2023-02-06 22:41:26 -0500

    Lab 3: added new detail page

---
+++ detail.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="eng">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Sophia Palma">
+    <meta name="description" content="Nurse Sharks">
+    <Title> Nurse Shark Facts</Title>
+  </head>
+
+  <body style="background-color:#D3D3D3">
+  <body>
+  <h1>Fun Facts About Nurse Sharks!</h1>
+        <nav>
+          <a href="https://spalma1.w3.uvm.edu/cs008/">Home</a>
+          <a href="index.php">Fishing is Bad</a>
+          <a href="form.php">Important Survey</a>
+        </nav>
+        <main>
+          <section>
+            <h2> Description</h2>
+            <p> Nurse sharks are common sea dwellers in the shallower parts of the west Atlantic and eastern Pacific oceans. They are light brown, and goldish colors. Something quite interesting about this species of shark, is that they are smooth to the touch and harmless to humans. It is true that they have sharp teeth and can get up to 14 feet long, however, they steer clear from humans.</p>
+            <figure>
+              <img alt="nurse sharks holding hands" src="Images/Nurse_sharks.png">
+              <figcaption> two nurse sharks holding hands </figcaption>
+              <!-- pov me and my boyfriend-->
+            </figure>
+          </section>
+
+          <section>
+            <h2> diet </h2>
+            <p> Nurse sharks are carnivores, meaning they prey on other sea creatures, rather than plants. Since nurse sharks swim towards the sea floor, they tend to eat smaller sea life. </p>
+            <style>
+              ul {
+                display: inline-block;
+                border: 1px solid #000;
+                padding: 20px;
+
+              }
+
+            </style>
+            <ul>
+              <li> shrimp </li>
+              <li> small fish </li>
+              <li> squid </li>
+              <li> shellfish </li>
+              <li><a href="https://www.nationalgeographic.com/animals/fish/facts/nurse-shark">source</a></li>
+            </ul>
+          </section>
+          <section>
+            <h2> nurse shark predators</h2>
+                <p> Nurse sharks are typically not preyed on, however, some larger sea life try to eat them. A good example of some predators are; lemon, tiger, bull, and hammerhead sharks.</p>
+          </section>
+        </main>
+    </body>
+</html>
+
